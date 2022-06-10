ee46753a37140cba7dc942c6e1f739632ea9125c
Johnny Yeng <a2902793@gmail.com>
2022-06-10 15:15:22 +0800

    Modified

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 # Copyright 2020 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,86 +12,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ctypes import sizeof
 import os
 import logging
 import random
 from flask import Flask, request
 
-#----------------------------
-# from pprint import pformat
-# import coloredlogs, logging
-# coloredlogs.install()
-# import re
-#----------------------------
+from modal import Player, get_throw, defend_or_move, DIRECTIONS
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-escape = ['F', 'L', 'R']
-
-@app.route("/", methods=['GET'])
-def index():
-    return "Let the battle begin!"
 
 @app.route("/", methods=['POST'])
 def move():
     request.get_data()
-    data = request.json
+    logger.info(request.json)
 
-    # logger.info(pformat(request.json['arena']['state']))
-    my_player = data['_links']['self']['href']
-    my_status = {
-        'x': data['arena']['state'][my_player]['x'],
-        'y': data['arena']['state'][my_player]['y'],
-        'direction': data['arena']['state'][my_player]['direction']
-    }
-    
-    columns = data['arena']['dims'][0] + 1
-    rows = data['arena']['dims'][1] + 1
-    arena = [[0 for _ in range(rows)] for _ in range(columns)] 
-    # pattern = "\-([a-zA-Z0-9]*)\-[a-zA-Z0-9]*.a.run.app"
+    MYSELF_URL = request.json['_links']['self']['href']
+    STATES = request.json['arena']['state']
+    MYSELF = STATES[MYSELF_URL]
 
-    for player in data['arena']['state']:
-        # player_id = re.search(pattern, player).group(1)[:3]
-        # print(player_id)
-        player_x = data['arena']['state'][player]['x']
-        player_y = data['arena']['state'][player]['y']
-        # arena[player_x][player_y] = player_id
-        arena[player_x][player_y] = data['arena']['state'][player]['direction']
+    myself = Player(MYSELF['x'], MYSELF['y'], MYSELF['direction'], MYSELF['wasHit'], MYSELF['score'])
+
+
+    for state_url, data in STATES.items():
+      if not state_url == MYSELF_URL:
+        opponent = Player(data['x'], data['y'], data['direction'], data['wasHit'], data['score'])
+        if abs(myself.x - opponent.x) <= 3 and abs(myself.y - opponent.y) <= 3:
+          response = get_throw(myself=myself, opponent=opponent)
+          if response:
+            return response
+          else:
+            return defend_or_move(myself=myself, opponent=opponent)
     
-    enemy_flag = False
-    if my_status['direction'] == 'N':
-        lower_bound = 0 if my_status['y'] < 3 else my_status['y'] - 3
-        for enemy in arena[my_status['x']][lower_bound:my_status['y']]:
-            if enemy == 'S':
-                return 'T'
-        else:
-            return escape[random.randrange(len(escape))]
-    elif my_status['direction'] == 'S':
-        upper_bound = len(arena[0]) if my_status['y'] > len(arena[0] - 3) else my_status['y'] + 3
-        for enemy in arena[my_status['x']][my_status['y']:upper_bound]:
-            if enemy == 'N':
-                return 'T'
-        else:
-            return escape[random.randrange(len(escape))]
-    elif my_status['direction'] == 'W':
-        lower_bound = 0 if my_status['x'] < 3 else my_status['x'] - 3
-        for enemy in arena[lower_bound:my_status['x']]['y']:
-            if enemy == 'E':
-                return 'T'
-        else:
-            return escape[random.randrange(len(escape))]
-    else: # 'E'
-        upper_bound = len(arena) if my_status['x'] > len(arena) - 3 else my_status['x'] + 3
-        for enemy in arena[my_status['x']:upper_bound]['y']:
-            if enemy == 'W':
-                return 'T'
-        else:
-            return escape[random.randrange(len(escape))]
+    return DIRECTIONS[random.randrange(len(DIRECTIONS))]
+
+
 
 if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
-    # app.run(debug=True,host='127.0.0.1',port=int(os.environ.get('PORT', 8080)))
-
+  app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
